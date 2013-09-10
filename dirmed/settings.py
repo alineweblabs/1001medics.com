893c206319177158ea2204c1cb704eee356b0659
robertuj <robertuj@gmail.com>
2013-09-10 00:29:56 -0600

    Update Settings.py admin, layout edit STATIC_URL, add .ignore

--- dirmed/settings.py
+++ dirmed/settings.py
@@ -1,13 +1,12 @@
 #encoding:utf-8
 import os
 
-
 DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Roberto Urita Jimenez', 'robertuj@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -56,7 +55,7 @@ MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'media/'))
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = 'http://localhost:8000/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,7 +65,7 @@ STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://127.0.0.1:8000/static/'
+STATIC_URL = 'http://localhost:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
